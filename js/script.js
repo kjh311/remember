a2751fc73997d00c8ead895745dbb76277068766
Kevin Huelsmann <kjh311@gmail.com>
2015-10-10 11:11:39 -0700

    first commit, basic layout done, randomizer works

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+    // alert( "jquery works!!!!!" );
+
+var places = ["England", "France", "Italy", "Switzerland", "Italy", "Germany", "Greeece", "Austria", "Netherlands", "Belgium", "Denmark", "Sweden", "Ireland", "Spain", "Croatia", "Serbia"];
+
+    // alert( "jquery works!!!!!" );
+
+
+//function that shuffles the array
+  function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+$('.btn').click(function() {
+  shuffle(places);
+  var item = places[0];
+  $('span').html(item);
+});
+
+});
